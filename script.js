78782acd699d3c9171b3541340477a1806437cee
vitorf2025 <vitor.franca21@escola.pr.gov.br>
2025-03-24 11:45:59 -0300

    .

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
-<body>
-    <header class=" p-5">
-        
-        <nav class="container d-flex justify-content-between align-items-center" >
-            <img src="img/logo.png" class="nav-img" loading="lazy">
-            <ul class="nav mt-5">
-                <li class="nav-item"><a class="nav-link" href="#inicio">Início</a></li>
-                <li class="nav-item"><a class="nav-link" href="#galeria">Galeria</a></li>
-                <li class="nav-item"><a class="nav-link" href="#contato">Contato</a></li>
-            </ul>
-                <div id="acessibilidade">
-                    <button id="aumentar-fonte" class="btn btn-primary fw-bold">A+</button>
-                    <button id="diminuir-fonte" class="btn btn-primary fw-bold">A-</button>
-                </div>
-        </nav>
+document.addEventListener('DOMContentLoaded', function(){
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+
+    let tamanhoAtualFonte = 1;
+    aumentaFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte += 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+
+    });
+});
