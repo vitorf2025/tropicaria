cee9e520ebb0b052ef9d1d14e932ae91872e9889
vitorf2025 <vitor.franca21@escola.pr.gov.br>
2025-03-24 11:43:28 -0300

    .

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-document.addEventListener ('DOMContentLoaded',function(){
-    const aumentaFonteBotao = document.getElementById ('aumentar-fonte');
-    const aumentaFonteBotao = document.getElementById ('diminuir-fonte');
+//Código omitido
 
-    let tamanhoAtualFonte = 1;
-    aumentaFonteBotao.addEventListener('click', function(){
-        tamanhoAtualFonte +-
-        document.body.style.fontSize = '${tamanhoAtualFonte}rem'
-
-    })
-
-    diminuirFonteBotao.addEventListener('click', function(){
-        tamanhoAtualFonte +-
-        document.body.style.fontSize = '${tamanhoAtualFonte}rem'
+<body>
+    <header class=" p-5">
         
-    })
+        <nav class="container d-flex justify-content-between align-items-center" >
+            <img src="img/logo.png" class="nav-img" loading="lazy">
+            <ul class="nav mt-5">
+                <li class="nav-item"><a class="nav-link" href="#inicio">Início</a></li>
+                <li class="nav-item"><a class="nav-link" href="#galeria">Galeria</a></li>
+                <li class="nav-item"><a class="nav-link" href="#contato">Contato</a></li>
+            </ul>
+                <div id="acessibilidade">
+                    <button id="aumentar-fonte" class="btn btn-primary fw-bold">A+</button>
+                    <button id="diminuir-fonte" class="btn btn-primary fw-bold">A-</button>
+                </div>
+        </nav>
+</header>
 
-)}
