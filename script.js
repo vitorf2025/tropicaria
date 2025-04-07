4854415a1111ad0b195e04c826bd4fee9ec1e4e2
vitorf2025 <vitor.franca21@escola.pr.gov.br>
2025-04-07 11:17:31 -0300

    .

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ document.addEventListener('DOMContentLoaded', function () {
         botaoDeAcessibilidade.classList.toggle('rotacao-botao');
         opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
 
+        const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
+        botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado)
     })
 
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
