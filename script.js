d4bf76cbaf139430d3a11e9b32b91460ad3f6b27
vitorf2025 <vitor.franca21@escola.pr.gov.br>
2025-04-14 11:10:14 -0300

    .

--- script.js
+++ script.js
@@ -36,3 +36,8 @@ document.addEventListener('DOMContentLoaded', function(){
  
  
  })
+
+ ScrollReveal().reveal('#inicio', { delay: 500 });
+ ScrollReveal().reveal('#tropicalia', { delay: 500 });
+ ScrollReveal().reveal('#galeria', { delay: 500 });
+ ScrollReveal().reveal('#contato', { delay: 500 });
